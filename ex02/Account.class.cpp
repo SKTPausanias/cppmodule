89367c21dc84f14717d924cdefe85d1d83075396
Miguel Laplana marin <mlaplana@c3r1s5.42madrid.com>
2020-03-07 13:47:20 +0100

    queda02

---
+++ ex02/Account.class.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.class.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaplana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 12:41:58 by mlaplana          #+#    #+#             */
+/*   Updated: 2020/03/07 12:42:03 by mlaplana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+#include "Account.class.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account::getNbAccounts(void)
+{
+	return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return Account::_totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return Account::_totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void Account::_displayTimestamp(void)
+{
+
+}
+
+void Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << Account::_nbAccounts << ";";
+	std::cout << "total:" << Account::_totalAmount << ";";
+	std::cout << "deposits:" << Account::_totalNbDeposits << ";";
+	std::cout << "withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+}
