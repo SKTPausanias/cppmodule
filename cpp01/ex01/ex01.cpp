2dd8578aaea461e6e2acef365bde01e4f2d1ad29
Miguel Laplana marin <mlaplana@c3r2s6.42madrid.com>
2020-03-08 14:17:46 +0100

    cpp01

---
+++ cpp01/ex01/ex01.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaplana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/08 14:11:14 by mlaplana          #+#    #+#             */
+/*   Updated: 2020/03/08 14:11:16 by mlaplana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+void memoryLeak()
+{
+	std::string* panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
+	delete panther;
+}
