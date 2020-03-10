54cbf3a9e01d0a60f3cdf9ad4a3b53a3d1f9a972
Miguel Laplana marin <mlaplana@c3r2s4.42madrid.com>
2020-03-10 16:56:27 +0100

    cpp00

--- cpp01/ex05/Brain.cpp
+++ cpp01/ex05/Brain.cpp
@@ -6,11 +6,12 @@
 /*   By: mlaplana <mlaplana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 19:17:08 by mlaplana          #+#    #+#             */
-/*   Updated: 2020/03/10 12:19:28 by mlaplana         ###   ########.fr       */
+/*   Updated: 2020/03/10 16:56:10 by mlaplana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
+#include <sstream>
 
 int Brain::get_consistency() const
 {
@@ -19,5 +20,7 @@ int Brain::get_consistency() const
 
 std::string Brain::identify() const
 {
-	
+	std::stringstream str;
+	str << std::hex << std::showbase << (unsigned long)this;
+	std::cout << str.str();
 }
