a61c1bc817d75e1508c51067ab93a6bb31aa63b5
Anias <miguellaplanamarin@gmail.com>
2020-07-09 14:27:22 +0200

    probando exam

--- exam04/minishell.c
+++ exam04/minishell.c
@@ -2,6 +2,27 @@
 #include <unistd.h>
 #include <string.h>
 
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
+
+char *ft_strdup(char *str)
+{
+	char *new = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	int i = 0;
+	while (str[i])
+	{
+		new[i] = str[i];
+		i++;
+	}
+	new[i] = '\0';
+	return new;
+}
+
 int strchr2(int start, int end, char **av, char *str)
 {
 	while (start < end)
@@ -13,28 +34,66 @@ int strchr2(int start, int end, char **av, char *str)
 	return 0;
 }
 
-int exec_cmd(int start, int end, char **av, char **env)
+int do_cmd(int i, int end, char **av, char **env)
 {
-	if (strcmp(av[start], ";") == 0)
+	char **args = malloc(sizeof(char*) * ((end - i) + 1));
+	int j = i;
+	while (j < end)
+	{
+		args[j] = ft_strdup(av[j]);
+		j++;
+	}
+	args[j] = "NULL";
+	if (strcmp(av[i], "cd") == 0)
+	{
+		if ((end - i) != 1)
+			printf("bad arguments\n");
+		else if (chdir(av[i + 1]))
+			printf("error: cd: cannot change directory to \n");
+	}
+	else
+		execve(av[i], args, env);
+}
+
+int ft_pipe(int i, int end, char **av, char **env)
+{
+	int fd[2];
+	pipe(fd);
+	pid_t pid;
+
+	if (!fork())
+	{
+		dup2(fd[1], 1);
+		do_cmd(i, end, av, env);
+		exit(0);
+	}
+	else
+	{
+		wait(NULL);
+		dup2(fd[0], 0);
+        close(fd[1]);
+	}
+}
+
+int exec_cmd(int i, int end, char **av, char **env)
+{
+	int start = i;
+	if (strcmp(av[i], ";") == 0)
 		return 0;
-	/*if (strchr2(start, end, av, "|") == 1)
+	while (i < end)
 	{
-
-	}*/
-	while (start < end)
-	{
-		if (strchr2(start, end, av, "|") == 1)
+		if (strchr2(i, end, av, "|") == 1)
 		{
-
+			start = i;
+			while (strcmp(av[i], "|") != 0)
+				i++;
+			ft_pipe(start, i, av, env);
+			start = i + 1;
 		}
 		else
-		{
-			
-		}
-		/*printf("%s ", av[start]);
-		start++;*/
+			do_cmd(i, end, av, env);
+		i++;
 	}
-	printf("\n");
 	return 1;
 }
 
