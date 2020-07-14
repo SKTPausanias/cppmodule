cf8a4482e88266856bfbac7365052f7ab6ecbbef
Miguel Laplana marin <mlaplana@c1r3s3.42madrid.com>
2020-07-14 20:40:17 +0200

    cpp04 ex01

--- cpp04/ex02/Squad.hpp
+++ cpp04/ex02/Squad.hpp
@@ -6,7 +6,22 @@
 /*   By: mlaplana <mlaplana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 18:26:18 by mlaplana          #+#    #+#             */
-/*   Updated: 2020/07/14 18:26:19 by mlaplana         ###   ########.fr       */
+/*   Updated: 2020/07/14 20:29:43 by mlaplana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SQUAD_HPP
+#define SQUAD_HPP
+
+#include "ISquad.hpp"
+
+class Squad
+{
+public:
+	virtual ~Squad() {}
+	virtual int getCount() const = 0;
+	virtual ISpaceMarine* getUnit(int) const = 0;
+	virtual int push(ISpaceMarine*) = 0;
+};
+
+#endif
