140dcea5682abced558450abf8f8380d34b5f317
Anias <miguellaplanamarin@gmail.com>
2020-08-14 19:43:25 +0200

    cpp05 ex04

--- cpp05/ex04/OfficeBlock.cpp
+++ cpp05/ex04/OfficeBlock.cpp
@@ -6,7 +6,72 @@
 /*   By: mlaplana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/14 13:26:56 by mlaplana          #+#    #+#             */
-/*   Updated: 2020/08/14 13:26:57 by mlaplana         ###   ########.fr       */
+/*   Updated: 2020/08/14 19:38:44 by mlaplana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "OfficeBlock.hpp"
+
+OfficeBlock::OfficeBlock(): _intern(nullptr), _signer(nullptr), _executor(nullptr)
+{    
+}
+
+OfficeBlock::OfficeBlock(Intern &intern, Bureaucrat &signer, Bureaucrat &executor):
+                        _intern(&intern), _signer(&signer), _executor(&executor)
+{
+}
+
+OfficeBlock::~OfficeBlock()
+{   
+}
+
+const char* OfficeBlock::NoInternException::what() const throw() 
+{
+    return "Can't find the intern!";   
+}
+
+const char* OfficeBlock::NoSignerException::what() const throw()
+{
+    return "Can't find a signer!";
+}
+
+const char* OfficeBlock::NoExecutorException::what() const throw()
+{
+    return "Can't find a executor!";
+}
+
+void OfficeBlock::setIntern(Intern &intern)
+{
+    this->_intern = &intern;
+}
+
+void OfficeBlock::setSigner(Bureaucrat &signer)
+{
+    this->_signer = &signer;
+}
+
+void OfficeBlock::setExecutor(Bureaucrat &executor)
+{
+    this->_executor = &executor;
+}
+
+void OfficeBlock::doBureaucracy(std::string name, std::string target) const
+{
+    if (!this->_intern)
+        throw OfficeBlock::NoInternException();
+    Form *form = _intern->makeForm(name, target);
+    if (!form)
+        return;
+    if (!this->_signer)
+    {
+        delete form;
+        throw OfficeBlock::NoSignerException();
+    }
+    _signer->signForm(*form);
+    if (!this->_executor)
+    {
+        delete form;
+        throw OfficeBlock::NoExecutorException();
+    }
+    _executor->executeForm(*form);
+}
--- cpp05/ex04/main.cpp
+++ cpp05/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlaplana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/03 13:38:10 by mlaplana          #+#    #+#             */
-/*   Updated: 2020/08/14 12:43:46 by mlaplana         ###   ########.fr       */
+/*   Updated: 2020/08/14 19:42:41 by mlaplana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,49 @@
 #include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "Intern.hpp"
+#include "OfficeBlock.hpp"
 
 int main()
 {
     Intern becario;
-    Form *formulario;
+    Bureaucrat danny = Bureaucrat("danny ocean", 37);
+    Bureaucrat loco = Bureaucrat("locooo", 12);
+    OfficeBlock ob;
 
-    formulario = becario.makeForm("robotomy request", "Bender");
-    delete formulario;
-    formulario = becario.makeForm("presidential pardon", "Bender");
-    delete formulario;
-    formulario = becario.makeForm("shrubbery creation", "Bender");
-    delete formulario;
-    formulario = becario.makeForm("coffee", "Bender");
-    delete formulario;
+    ob.setIntern(becario);
+    ob.setSigner(loco);
+    ob.setExecutor(danny);
+
+    try
+    {
+        ob.doBureaucracy("robotomy request", "uuu");
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+    std::cout << "\n  -----  \n\n";
+
+    try
+    {
+        ob.doBureaucracy("presidential pardon", "mommy");
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+    std::cout << "\n  -----  \n\n";
+
+    try
+    {
+        ob.doBureaucracy("new form lulw", "mommy");
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
