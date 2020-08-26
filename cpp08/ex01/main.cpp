3330b0b12617e493ba47f8846b6b136b258ac65f
Anias <miguellaplanamarin@gmail.com>
2020-08-26 18:55:01 +0200

    cpp08 ex01

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mlaplana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 18:55:41 by mlaplana          #+#    #+#             */
-/*   Updated: 2020/08/25 16:10:03 by mlaplana         ###   ########.fr       */
+/*   Updated: 2020/08/26 18:54:14 by mlaplana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int main()
 {
+
+    srand(time(NULL));
+    
     Span sp = Span(5);
     sp.addNumber(5);
     sp.addNumber(3);
@@ -22,4 +25,22 @@ int main()
     sp.addNumber(11);
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
+
+    Span sp2 = Span(1);
+    sp2.addNumber(2);
+
+    try
+    {
+        std::cout<< sp2.shortestSpan() << std::endl;
     }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    Span sp3 = Span(200000);
+    for (int i = 0; i < 200000; i++)
+        sp3.addNumber(rand() % 500000);
+    std::cout << sp3.shortestSpan() << std::endl;
+    std::cout << sp3.longestSpan() << std::endl;
+}
